41bfd943f6f6e0aa8b9743c3d6235e0dbc8cf81c
hye0n9-rae <luke0725hr@gmail.com>
2026-03-25 17:04:43 +0900

    C언어 과제

---
+++ 조형래_2025504323.c
@@ -0,0 +1,53 @@
+#include <stdio.h> 
+
+int main()
+{
+	/*
+	
+	//문제 2//
+
+	int num; // num으로 정수 변수 설정
+
+	printf("정수 입력: "); // 정수 입력: 문구 출력
+	scanf("%d", &num); //num변수에 정수 입력받기
+
+	printf("8진수: %o\n", num); //8진수로 입력받은 값 출력
+	printf("10진수: %d\n", num); //10진수로 입력받은 값 출력
+	printf("16진수(대문자): %X\n", num); //16진수로 입력받은 값 대문자로 출력
+	printf("16진수(소문자): %x\n", num); //16진수로 입력받은 값 소문자로 출력
+	
+
+	//문제 3//
+
+	int month; // month로 월급 값 변수 설정
+	int year; //year로 연봉 값 변수 설정
+
+	printf("월급: "); //월급: 출력
+	scanf("%d", &month); //월급 값 정수로 입력받기
+
+	year = month * 12; //연봉 = 월급 * 12 계산
+
+	printf("연봉: %d\n", year); //연봉 값 출력
+
+
+	//문제 5//
+
+	int a, b; //a,b로 정수 변수 설정
+
+	printf("첫번째 양의 정수를 입력하세요: "); //첫번째 양의 정수를 입력하세요: 출력
+	scanf("%d", &a); //a에 첫번쨰 정수 입력받음
+
+	printf("두번째 양의 정수를 입력하세요: "); //두번째 양의 정수를 입력하세요: 출력
+	scanf("%d", &b); //b에 두번째 정수 입력받기
+
+	printf("덧셈: %d\n", a + b); //a+b 값을 출력
+	printf("뺄셈: %d\n", a - b); //a-b 값을 출력
+	printf("곱셈: %d\n", a * b); //a*b 값을 출력
+	printf("나눗셈: %d\n", a / b); //a/b 값을 출력
+	printf("나머지: %d\n", a % b); //a%b 값을 출력
+
+
+	*/
+
+	return 0;
+}
